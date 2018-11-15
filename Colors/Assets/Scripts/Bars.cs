a9281e5de9135d1e11132595202ba82474ef48a8
Solange Valverde <solange.valverde@gmail.com>
2018-11-14 18:26:20 -0800

    Level 0.0, 0.1 and 0.2 (tutorial)

---
+++ Colors/Assets/Scripts/Bars.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bars : MonoBehaviour
+{
+    //    Player playerFromClass;
+    // Use this for initialization
+    GameObject player;
+    SpriteRenderer m_SpriteRenderer;
+    //The Color to be assigned to the Renderer’s Material
+    BoxCollider2D m_Collider;
+    public GameObject[] bar;
+
+
+    Color green, blue, red, yellow, MutedBlue, MutedGreen, MutedRed, MutedYellow;
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        // m_SpriteRenderer = player.GetComponent<SpriteRenderer>();
+
+        m_Collider = GetComponent<BoxCollider2D>();
+
+
+        bar = GameObject.FindGameObjectsWithTag("bar");
+
+
+        green = new Color(0f, 0.784f, 0f, 1f);
+        blue = new Color(0f, 0f, 0.784f, 1f);
+        red = new Color(0.784f, 0f, 0f, 1f);
+        yellow = new Color(0.784f, 0.784f, 0f, 1f);
+        MutedBlue = new Color(0f, 0f, 0.384f, 1f);
+        MutedGreen = new Color(0f, 0.384f, 0f, 1f);
+        MutedRed = new Color(0.384f, 0f, 0f, 1f);
+        MutedYellow = new Color(0.384f, 0.384f, 0f, 1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if player changes color then they mute colors
+        if (player.GetComponent<SpriteRenderer>().color == green)
+        {
+            // Debug.Log("player green " );
+            for (int i = 0; i < bar.Length; i++)
+            {
+                // Debug.Log("checking bars ");
+                if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == blue.ToString())
+                {
+                    //     Debug.Log("muted blue");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedBlue;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == red.ToString())
+                {
+                    //    Debug.Log("muted red");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedRed;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+                }
+
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == yellow.ToString())
+                {
+                    //   Debug.Log("muted yellow");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedYellow;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == MutedGreen.ToString())
+                {
+                    //   Debug.Log("reg green");
+                    bar[i].GetComponent<SpriteRenderer>().color = green;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = true;
+
+                }
+
+            }
+        }
+        if (player.GetComponent<SpriteRenderer>().color == blue)
+        {
+            //Debug.Log("player green " );
+            for (int i = 0; i < bar.Length; i++)
+            {
+                // Debug.Log("checking bars ");
+                if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == MutedBlue.ToString())
+                {
+                    //    Debug.Log("muted blue");
+                    bar[i].GetComponent<SpriteRenderer>().color = blue;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = true;
+
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == red.ToString())
+                {
+                    //  Debug.Log("muted red");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedRed;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == yellow.ToString())
+                {
+                    //  Debug.Log("muted yellow");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedYellow;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == green.ToString())
+                {
+                    //  Debug.Log("reg green");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedGreen;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+                }
+            }
+        }
+        if (player.GetComponent<SpriteRenderer>().color == red)
+        {
+            //Debug.Log("player green " );
+            for (int i = 0; i < bar.Length; i++)
+            {
+                // Debug.Log("checking bars ");
+                if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == blue.ToString())
+                {
+                    //  Debug.Log("muted blue");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedBlue;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == MutedRed.ToString())
+                {
+                    // Debug.Log("muted red");
+                    bar[i].GetComponent<SpriteRenderer>().color = red;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = true;
+
+                }
+
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == yellow.ToString())
+                {
+                    //  Debug.Log("muted yellow");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedYellow;
+
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == green.ToString())
+                {
+                    // Debug.Log("reg green");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedGreen;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+            }
+        }
+        if (player.GetComponent<SpriteRenderer>().color == yellow)
+        {
+            //Debug.Log("player green " );
+            for (int i = 0; i < bar.Length; i++)
+            {
+                // Debug.Log("checking bars ");
+                if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == blue.ToString())
+                {
+                    // Debug.Log("muted blue");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedBlue;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == red.ToString())
+                {
+                    // Debug.Log("muted red");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedRed;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == MutedYellow.ToString())
+                {
+                    // Debug.Log("muted yellow");
+                    bar[i].GetComponent<SpriteRenderer>().color = yellow;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = true;
+
+                }
+                else if (bar[i].GetComponent<SpriteRenderer>().color.ToString() == green.ToString())
+                {
+                    // Debug.Log("reg green");
+                    bar[i].GetComponent<SpriteRenderer>().color = MutedGreen;
+                    bar[i].GetComponent<BoxCollider2D>().enabled = false;
+
+                }
+            }
+        }
+
+
+    }
+    ///*
+
+    void Mute()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //  bar = GameObject.FindGameObjectsWithTag("bar");
+        //bar = this.gameObject;
+
+
+
+        if (collision.gameObject.name != "bar")
+        {
+            for (int i = 0; i < bar.Length; i++)
+            {
+
+                if (this.GetComponent<SpriteRenderer>().color.ToString() == player.GetComponent<SpriteRenderer>().color.ToString())
+                {
+
+                    m_Collider.enabled = true;
+
+                }
+                else
+                {
+                    m_Collider.enabled = false;
+
+                }
+            }
+        }
+
+    }
+
+    //  */
+
+}
---
+++ Colors/Assets/Scripts/Player.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer m_SpriteRenderer;
+    Color m_NewColor;
+    bool canJump;
+    private Rigidbody2D rb;
+    public GameObject[] door;
+    public GameObject[] floor;
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        canJump = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //movement
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (canJump && Input.GetKey(KeyCode.W))
+        {
+            // the cube is going to move upwards in 5 units per second
+            //rb.velocity = new Vector3(0, 10, 0);
+            rb.AddForce(new Vector2(0, 400));
+            canJump = false;
+            //  Debug.Log("jump");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //rb.velocity = new Vector3(3, 0, 0);
+            rb.AddForce(new Vector2(12, 0));
+            //  Debug.Log("RIGHT");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //rb.velocity = new Vector3(-3, 0, 0);
+            rb.AddForce(new Vector2(-12, 0));
+
+            //  Debug.Log("LEFT");
+        }
+
+        if (rb.velocity.y == 0)
+        {
+            rb.AddForce(new Vector2(0, -400));
+            canJump = true;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.AddForce(new Vector2(0, -10));
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //color change
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Set the Color to the values gained from the Sliders
+            m_NewColor = new Color(0.784f, 0f, 0f, 1f);
+            //m_NewColor = Color.red;
+            //Set the SpriteRenderer to the Color defined by the Sliders
+            m_SpriteRenderer.color = m_NewColor;
+            //  Debug.Log("red");
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            m_NewColor = new Color(0f, 0f, 0.784f, 1f);
+            m_SpriteRenderer.color = m_NewColor;
+            //  Debug.Log("blue");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            m_NewColor = new Color(0.784f, 0.784f, 0f, 1f);
+            m_SpriteRenderer.color = m_NewColor;
+            //  Debug.Log("yellow");
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            m_NewColor = new Color(0f, 0.784f, 0f, 1f);
+            m_SpriteRenderer.color = m_NewColor;
+            //  Debug.Log("green");
+        }
+
+
+    }
+
+
+    // called when this GameObject collides with GameObject2.
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        door = GameObject.FindGameObjectsWithTag("door");
+        floor = GameObject.FindGameObjectsWithTag("floor");
+        //bar = this.gameObject;
+
+
+        if (collision.gameObject == door[0])
+        {
+            Debug.Log("SceneManager.GetActiveScene().buildIndex " + SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if (collision.gameObject == floor[0])
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
